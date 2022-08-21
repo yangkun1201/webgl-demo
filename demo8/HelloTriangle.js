df8a9968bd3153eb4f213d1c9032703215a136df
yangkun <1149642605@qq.com>
2022-08-21 20:49:54 +0800

    add HelloTriangle

---
+++ demo8/HelloTriangle.js
@@ -0,0 +1,40 @@
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;' +
+    'void main() {' +
+    'gl_Position = a_Position;' +
+    '}'
+
+var FSHADER_SOURCE =
+    'void main(){' +
+    'gl_FragColor = vec4(1.0,0.0,0.0,1.0);' +
+    '}'
+
+function main() {
+    var canvas = document.getElementById('webgl')
+    var gl = getWebGLContext(canvas)
+    initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)
+    gl.clearColor(0.0,0.0,0.0,1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    var n = initVertexBuffers(gl)
+    gl.drawArrays(gl.TRIANGLE_STRIP,0,n)
+}
+
+function initVertexBuffers(gl) {
+    // 指定点坐标
+    var vertices = new Float32Array([-0.5,0.5,0.5,0.5,-0.5,-0.5,0.5,-0.5])
+    // 指定点个数
+    var n = 4
+    // 创建buffer
+    var vertexbuffer = gl.createBuffer()
+    // 绑定buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER,vertexbuffer)
+    // 向buffer传递数据
+    gl.bufferData(gl.ARRAY_BUFFER,vertices,gl.STATIC_DRAW)
+    // 获取顶点着色器变量地址
+    var a_Position = gl.getAttribLocation(gl.program,'a_Position')
+    // 关联buffer与着色器变量
+    gl.vertexAttribPointer(a_Position,2,gl.FLOAT,false,0,0)
+    gl.enableVertexAttribArray(a_Position)
+    return n
+}
+
